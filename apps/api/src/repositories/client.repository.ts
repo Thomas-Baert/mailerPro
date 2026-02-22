9052a453cec7264d97ff496efc601905ff6b3fb4
Thomas <thomas.baert@etu.umontpellier.fr>
2026-02-22 18:11:34 +0100

    feat: ajout repository client

---
+++ apps/api/src/repositories/client.repository.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@mailerpro/database';
+import { Prisma } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const findAllClients = async () => {
+    return prisma.client.findMany();
+}
+
+export const findClientById = async (id: string) => {
+    return prisma.client.findUnique({ where: { id: id } });
+}
+
+export const findClientByEmail = async (email: string) => {
+    return prisma.client.findUnique({ where: { email: email } });
+}
+
+export const createClient = async (data: Prisma.ClientCreateInput) => {
+    return prisma.client.create({ data });
+}
+
+export const deleteClient = async (id: string) => {
+    return prisma.client.delete({ where: { id: id } });
+}
+
+export const updateClient = async (id: string, data: Prisma.ClientUpdateInput) => {
+    return prisma.client.update({ where: { id: id }, data });
+}
