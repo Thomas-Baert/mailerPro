35d945857261eceabea575d40f7b32105bfb6d49
Thomas <thomas.baert@etu.umontpellier.fr>
2026-02-24 19:51:07 +0100

    feat: ajout routage dynamique

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,22 @@
 import MainLayout from './layouts/MainLayout';
 import Login from './pages/Login';
+import Home from './pages/Home';
 import './App.css';
 import Register from "./pages/Register.tsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <MainLayout>
-        <Login />
-        <Register />
-    </MainLayout>
+      <BrowserRouter>
+          <MainLayout>
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
+              </Routes>
+          </MainLayout>
+      </BrowserRouter>
   );
 }
 
