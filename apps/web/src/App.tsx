b3ffb142c7bfb21b4c24b04c443a13e824626aaf
Thomas <thomas.baert@etu.umontpellier.fr>
2026-02-24 19:59:49 +0100

    feat: ajout basename

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-      <BrowserRouter>
+      <BrowserRouter basename="/mailerpro">
           <MainLayout>
               <Routes>
                   <Route path="/" element={<Home />} />
