ef0970a147f7dcc7fdabbff7d6b747dc43229ffa
Thomas <thomas.baert@etu.umontpellier.fr>
2026-02-24 19:50:57 +0100

    feat: création d'une page home basique

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,10 +1,34 @@
 import styles from './Home.module.css';
+import {Link} from "react-router-dom";
+import {useQuery} from "@tanstack/react-query";
+import {getMe} from "../services/auth.service.ts";
 
 export default function Home() {
+    const token = localStorage.getItem('token');
+
+    const { data: user, isLoading, isError }: any = useQuery({
+        queryKey: ['me'],
+        queryFn: getMe,
+        enabled: !!token
+    });
+
+    function Username() {
+        if (isLoading) return <span>Loading...</span>;
+        else if (!token || isError) return <span>Not logged in</span>;
+        else return <span>{user.username}</span>;
+    }
+
     return (
         <div className={styles.container}>
             <h1>Bienvenue sur MailerPro</h1>
             <p>Ceci est votre nouvelle page d'accueil vide.</p>
+            <Link to="/login"><button>login</button></Link>
+            <Link to="/register"><button>register</button></Link>
+            <div>
+                <button>
+                    <Username />
+                </button>
+            </div>
         </div>
     );
 }
