020d8e582106d5fcb33512b79487d312f8a9370d
Thomas <thomas.baert@etu.umontpellier.fr>
2026-02-25 22:10:29 +0100

    feat: ajout routes admins

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,21 +3,29 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import './App.css';
 import Register from "./pages/Register.tsx";
+import AdminUniversities from "./pages/AdminUniversities.tsx";
+import RoleGuard from "./components/RoleGuard.tsx";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
-  return (
-      <BrowserRouter basename="/mailerpro">
-          <MainLayout>
-              <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-              </Routes>
-          </MainLayout>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter basename="/mailerpro">
+            <MainLayout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+
+                    <Route path="/admin/universities" element={
+                        <RoleGuard allowedRoles={['GLOBAL_ADMIN']}>
+                            <AdminUniversities />
+                        </RoleGuard>
+                    } />
+                </Routes>
+            </MainLayout>
+        </BrowserRouter>
+    );
 }
 
 export default App
