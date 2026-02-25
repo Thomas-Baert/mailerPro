6f53fcb9aafc9f3ec788f70276dcbfb1bc027c27
Thomas <thomas.baert@etu.umontpellier.fr>
2026-02-25 22:11:20 +0100

    feat: ajout service de création d'universités

--- apps/web/src/services/universities.service.ts
+++ apps/web/src/services/universities.service.ts
@@ -1,3 +1,7 @@
 import client from '../api/client.ts'
 
 export const getUniversities = () => client.get('/universities');
+
+export const createUniversity = (data: { name: string, logo?: string }) => {
+    return client.post('/universities', data);
+}
